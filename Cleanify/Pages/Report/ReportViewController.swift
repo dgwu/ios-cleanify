7069227654da7ac124cc7e47f4a42a8d280068c0
andre-tirta <andretirtawijaya@gmail.com>
2018-08-20 12:40:45 +0700

    tes add report module

---
+++ Cleanify/Pages/Report/ReportViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ReportViewController.swift
+//  Cleanify
+//
+//  Created by Andre Tirta Wijaya on 20/08/18.
+//  Copyright © 2018 Daniel Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class ReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getReportList()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getReportList(){
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
