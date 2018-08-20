76f011d09bf9c0944c72a0e03adeed9f977ddb7e
Kevin Wijaya <kevinwij96@gmail.com>
2018-08-20 10:43:39 +0700

    hello world from kevin

--- Cleanify/Pages/News/NewsViewController.swift
+++ Cleanify/Pages/News/NewsViewController.swift
@@ -12,7 +12,7 @@ class NewsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("hello world")
         // Do any additional setup after loading the view.
     }
 
