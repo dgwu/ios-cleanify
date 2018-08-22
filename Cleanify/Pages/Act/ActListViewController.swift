c55ff30cd7dc35bc8d1b25a5268c17d764795b81
dgwu <danielgunawanwu@gmail.com>
2018-08-22 18:27:35 +0700

    event detail with dummy data

--- Cleanify/Pages/Act/ActListViewController.swift
+++ Cleanify/Pages/Act/ActListViewController.swift
@@ -17,12 +17,13 @@ class ActListViewController: UIViewController, UITableViewDelegate, UITableViewD
     @IBOutlet weak var mapView: MKMapView!
     
     @IBOutlet weak var eventTableView: UITableView!
+    var selectedEvent = [String: Any]()
     
     let dummyEvents = [
         [
             "title" : "another test",
             "photo_url" : "uhuy",
-            "description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             "location_desc" : "AEON MALL BSD",
             "location_latitude" : -6.304715,
             "location_longitude" : 106.643997,
@@ -152,6 +153,7 @@ class ActListViewController: UIViewController, UITableViewDelegate, UITableViewD
     // MARK : Table View Delegate
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let dummyEvent = self.dummyEvents[indexPath.section]
+        self.selectedEvent = dummyEvent
         
         performSegue(withIdentifier: "ToDetail", sender: self)
     }
@@ -160,6 +162,7 @@ class ActListViewController: UIViewController, UITableViewDelegate, UITableViewD
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "ToDetail") {
             if let destination = segue.destination as? ActDetailViewController {
+                destination.event = self.selectedEvent
                 print("berhasil yey")
             } else {
                 print("failed")
--- Cleanify/Pages/ActDetail/ActDetailViewController.swift
+++ Cleanify/Pages/ActDetail/ActDetailViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class ActDetailViewController: UIViewController {
 
+    
+    @IBOutlet weak var eventImageView: UIImageView!
+    @IBOutlet weak var eventTitleLabel: UILabel!
+    @IBOutlet weak var eventDescriptionLabel: UILabel!
+    @IBOutlet weak var eventDatetimeLabel: UILabel!
+    @IBOutlet weak var eventLocationDescriptionLabel: UILabel!
+    
+    var event = [String : Any]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        displayDetail()
+    }
+    
+    func displayDetail() {
+        eventImageView.image = UIImage.init(named: "Profile Image")
+//        "title" : "another test",
+//        "photo_url" : "uhuy",
+//        "description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//        "location_desc" : "AEON MALL BSD",
+//        "location_latitude" : -6.304715,
+//        "location_longitude" : 106.643997,
+
+        if let eventTitle = self.event["title"] as? String {
+            eventTitleLabel.text = eventTitle
+        }
+        if let eventDescription = self.event["description"] as? String {
+            eventDescriptionLabel.text = eventDescription
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +47,4 @@ class ActDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
