9a24d42a6b3cc40a7813a3a572ab3b97ee1a1067
Kevin Wijaya <kevinwij96@gmail.com>
2018-08-23 11:41:57 +0700

    Headnews scroll view

--- Cleanify/Pages/News/NewsViewController.swift
+++ Cleanify/Pages/News/NewsViewController.swift
@@ -8,28 +8,44 @@
 
 import UIKit
 
-class NewsViewController: UIViewController {
-
+class NewsViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var pageController: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images: [String] = ["headnews0","headnews1","headnews2","headnews3"]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello world")
-        // Do any additional setup after loading the view.
+        pageController.numberOfPages = images.count
+        
+        for index in 0..<images.count{
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imgView = UIImageView(frame: frame)
+            imgView.image = UIImage(named: images[index])
+            self.scrollView.addSubview(imgView)
+            
+        }
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(images.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
+        
+        // Do any additional setup after loading the view, typically from a nib.
+         
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        var pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageController.currentPage = Int(pageNumber)
+        
     }
-    */
-
+    
 }
+
